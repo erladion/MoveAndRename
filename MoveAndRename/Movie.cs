f6f239e99201eab97247c08ee1ab0bd548c7a083
erladion <johan.jansson94@gmail.com>
2018-12-21 08:20:08 +0100

    Added the files for movies

---
+++ MoveAndRename/Movie.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoveAndRename
+{
+	class Movie
+	{
+		public string Name;
+		public int Year;
+		public string Extension { get; set; }
+		public string CurrentPath { get; set; }
+		public string SubtitlePath { get; set; }
+		public bool GotSubtitle { get; set; }
+
+		public Movie()
+		{
+
+		}
+
+		public Movie(string name, int year)
+		{
+			this.Name = name;
+			this.Year = year;
+		}
+
+		public Movie(string name, string year)
+		{
+			this.Name = name;
+			this.Year = Convert.ToInt32(year);
+		}
+
+		public void PrintMovie()
+		{
+			Debug.WriteLine("---Printing movie info---");
+			Debug.WriteLine(Name);
+			Debug.WriteLine(Year);
+			Debug.WriteLine(CurrentPath);
+			Debug.WriteLine(Extension);
+			Debug.WriteLine("Got subtitle: " + GotSubtitle);
+			Debug.WriteLine("Subtitle path: " + SubtitlePath);
+			Debug.WriteLine("---Done printing movie info---");
+		}
+	}
+}
---
+++ MoveAndRename/MovieUtility.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMDbLib.Client;
+using TMDbLib.Objects.General;
+using TMDbLib.Objects.Search;
+using System.IO;
+using Shell32;
+
+namespace MoveAndRename
+{
+	class MovieUtility
+	{
+		public static HashSet<Movie> FindMovieMatches(TMDbClient cli, Movie mov)
+		{
+			HashSet<Movie> res = new HashSet<Movie>();
+
+			SearchContainer<SearchMovie> results = cli.SearchMovieAsync(mov.Name).Result;
+
+			foreach (SearchMovie item in results.Results)
+			{
+				if(item.Title.Contains(mov.Name) && item.ReleaseDate.Value.Year == mov.Year)
+				{
+					Debug.WriteLine("Got it!");
+					Debug.WriteLine(item.Title);
+				}
+			}
+
+			return res;
+		}
+
+		public static Movie ConvertFileToMovie(string path)
+		{
+
+
+
+
+			return null;
+		}
+
+		public static Dictionary<int, KeyValuePair<string, string>> GetFileProps(string filename)
+		{
+			Shell shl = new Shell();
+			Folder fldr = shl.NameSpace(Path.GetDirectoryName(filename));
+			FolderItem itm = fldr.ParseName(Path.GetFileName(filename));
+			Dictionary<int, KeyValuePair<string, string>> fileProps = new Dictionary<int, KeyValuePair<string, string>>();
+			for (int i = 0; i < 100; i++)
+			{
+				string propValue = fldr.GetDetailsOf(itm, i);
+				if (propValue != "")
+				{
+					fileProps.Add(i, new KeyValuePair<string, string>(fldr.GetDetailsOf(null, i), propValue));
+				}
+			}
+			return fileProps;
+		}
+	}
+}
